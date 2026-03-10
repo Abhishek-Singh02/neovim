6329ddecc50a5b2989f24db7fc337ac88307bb07
abhishekscb <abhisheksingh@codebuddy.co>
2026-03-10 12:56:14 +0530

    feat: add lazydev, treesitter and which-key plugins

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,79 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        build = ":TSUpdate",
+        lazy = false,
+        event = {
+            "BufReadPost",
+            "BufNewFile",
+        },
+        dependencies = {
+            { "nvim-treesitter/nvim-treesitter-textobjects" },
+        },
+        keys = {
+            { "<C-Space>", desc = "Increment selection" },
+            { "<bs>", desc = "Decrement selection", mode = "x" },
+        },
+        cmd = { "TSUpdateSync" },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                sync_install = false,
+                auto_install = true,
+                ensure_installed = {
+                    "bash",
+                    "fish",
+                    "c",
+                    "html",
+                    "fish",
+                    "javascript",
+                    "json",
+                    "lua",
+                    "luadoc",
+                    "luap",
+                    "markdown",
+                    "markdown_inline",
+                    "query",
+                    "regex",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "yaml",
+                    "dockerfile",
+                    "http",
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-Space>",
+                        node_incremental = "<C-Space>",
+                        scope_incremental = false,
+                        node_decremental = "<bs>",
+                    },
+                },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = {
+                    enable = true,
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ap"] = "@parameter.outer",
+                            ["ip"] = "@parameter.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                        },
+                    },
+                },
+            })
+        end,
+    }
+}
